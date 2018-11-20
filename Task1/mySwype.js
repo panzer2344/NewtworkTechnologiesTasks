38cd2fac5e11874686f0e644f9450af16afedb2f
alex <lexathebest23@gmail.com>
2018-11-20 12:29:40 +0300

    added main functions, but need to edit case - hints to second word in textbox

--- Task1/mySwype.js
+++ Task1/mySwype.js
@@ -1,13 +1,78 @@
 var swypeInputClassName = "swypeInput";
+var swypeDataListClassName = "swypeDataList"
+var swypeInputTextBoxClassName = "swypeInputTextBox";
+var swypeDataListOptionClassName = "swypeDLOption";
+var swypeDataListOptionIdNameBegin = "DLO";
 var swypeInputClass = document.getElementsByClassName(swypeInputClassName);
+var swypeDataListLen = 10;
+
+function findFirstWithSameBeginning(diseredBeginning){
+	var indexInDict = 0;
+	
+	while(indexInDict < dictionary.length){
+		if( dictionary[indexInDict].indexOf(diseredBeginning) == 0 ){
+			return indexInDict;
+		}
+		indexInDict++;
+	}
+	
+	return -1;	
+}
+
+function genDataList(diseredBeginning){
+	var indexInDict = -1;
+	var indexInDL = 0;
+	var foundedStrings = [];
+	
+	indexInDict = findFirstWithSameBeginning(diseredBeginning);
+	console.log(indexInDict);
+	
+	if( indexInDict == -1 ){
+		return foundedStrings;
+	}
+	
+	while(indexInDL < swypeDataListLen){
+		if( dictionary[indexInDict + indexInDL].indexOf(diseredBeginning) == 0 ){
+			foundedStrings.push(dictionary[indexInDict + indexInDL]);
+			indexInDL++;
+		}else{
+			break;
+		}
+	}
+	
+	return foundedStrings;
+}
 
 function swypeInputHandler(){
-	alert("you tap swype input textbox");
+	var foundedBeginOfString = this.value;
+	var currentSwypeInputDiv = this.parentNode;
+	var currentSwypeInputDivID = this.id;
+	var currentSwypeDataList = currentSwypeInputDiv.getElementsByClassName(swypeDataListClassName)[0];
+	
+	var hintsList = genDataList(foundedBeginOfString);
+	console.log(hintsList);
+	
+	while(currentSwypeDataList.firstChild){
+		currentSwypeDataList.removeChild(currentSwypeDataList.firstChild);
+	}
+	
+	for(var i = 0; i < hintsList.length; i++){
+		var option = document.createElement('option');
+		option.class = swypeDataListOptionClassName;
+		option.id = currentSwypeInputDivID + '_' + swypeDataListOptionIdNameBegin + i;
+		option.innerHTML = hintsList[i];
+		console.log(option);
+		currentSwypeDataList.appendChild(option);
+	}
 }
 
 window.onload = function(){
 	console.log(swypeInputClass);
 	for (swypeInputElem of swypeInputClass){
-		swypeInputElem.oninput = swypeInputHandler;
+		console.log(swypeInputElem.getElementsByClassName(swypeInputTextBoxClassName));
+		swypeInputElem.getElementsByClassName(swypeInputTextBoxClassName)[0].oninput = swypeInputHandler;
+		swypeInputElem.getElementsByClassName(swypeInputTextBoxClassName)[0].onclick = function() {
+			this.select();
+		}
 	}
 }
